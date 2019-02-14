e48bb7426b6f3e37efc0002c51e069d9b735895b
Lúcás Meier <cronokirby@gmail.com>
2019-02-14 20:22:57 +0100

    Add basic orthogonal camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,122 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::time::{Duration, Instant};
 
 #[macro_use]
 extern crate glium;
 use glium::{Display, glutin, index, Surface, VertexBuffer};
-use glium::glutin::{Event, VirtualKeyCode, WindowEvent};
+use glium::glutin::{ElementState, Event, VirtualKeyCode, WindowEvent};
+
+mod math;
+use math::Vec3;
+
+
+#[derive(Debug)]
+struct Controls {
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool    
+}
+
+impl Controls {
+    fn new() -> Self {
+        Controls { left: false, right: false, up: false, down: false }
+    }
+
+    fn update(&mut self, event: &WindowEvent) {
+        if let WindowEvent::KeyboardInput { input, .. } = event {
+            let toggle = input.state == ElementState::Pressed;
+            match input.virtual_keycode {
+                Some(VirtualKeyCode::A) => self.left = toggle,
+                Some(VirtualKeyCode::D) => self.right = toggle,
+                Some(VirtualKeyCode::W) => self.up = toggle,
+                Some(VirtualKeyCode::S) => self.down = toggle,
+                _ => {}
+            }
+        }
+    }
+}
+
+/// A struct for the first person Camera
+struct Camera {
+    /// Horizontal angle
+    theta: f32,
+    /// Vertical angle
+    phi: f32,
+    up: Vec3,
+    right: Vec3,
+    forward: Vec3,
+    /// The position of the Camera in the global space
+    position: Vec3,
+    /// The controls for the Camera
+    controls: Controls
+}
+
+impl Camera {
+    fn from_angles(theta: f32, phi: f32, position: Vec3) -> Self {
+        let sin_theta = theta.sin();
+        let cos_theta = theta.cos();
+        let sin_phi = phi.sin();
+        let cos_phi = phi.cos();
+
+        let forward = Vec3::new(
+            cos_phi * sin_theta,
+            -sin_phi,
+            cos_phi * cos_theta
+        );
+        let right = Vec3::new(
+            cos_theta,
+            0.0f32,
+            -sin_theta
+        );
+        let up = forward.cross(right).norm();
+
+        Camera { 
+            theta, phi, up, right, forward, position,
+            controls: Controls::new()
+        }
+    }
+
+    fn update(&mut self, dt: f32, event: &WindowEvent) {
+        let move_speed = 2.0 * dt;
+        self.controls.update(event);
+        if self.controls.left {
+            self.position -= self.right * move_speed;
+        }
+        if self.controls.right {
+            self.position += self.right * move_speed;
+        }
+        if self.controls.up {
+            self.position += self.forward * move_speed;
+        }
+        if self.controls.down {
+            self.position -= self.forward * move_speed;
+        }
+    }
+
+    fn get_up(&self) -> [f32; 3] {
+        self.up.uniform()
+    }
+
+    fn get_right(&self) -> [f32; 3] {
+        self.right.uniform()
+    }
+
+    fn get_forward(&self) -> [f32; 3] {
+        self.forward.uniform()
+    }
+
+    fn get_position(&self) -> [f32; 3] {
+        self.position.uniform()
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Camera::from_angles(0.0, 0.0, Vec3::new(0.0, 0.0, 0.0))
+    }
+}
 
 
 #[derive(Copy, Clone)]
@@ -43,15 +155,16 @@ fn main() {
     ];
     let vertex_buffer = VertexBuffer::new(&display, &vertices).unwrap();
 
+    let mut camera = Camera::default();
+    let mut then = Instant::now();
+
     loop {
         let mut target = display.draw();
         let uniforms = uniform! {
-            g_theta: 0.0f32,
-            g_phi: 0.0f32,
-            g_cam_up: [0.0, 1.0, 0.0f32],
-            g_cam_right: [1.0, 0.0, 0.0f32],
-            g_cam_forward: [0.0, 0.0, 1.0f32],
-            g_eye: [0.0, 0.0, -2.0f32],
+            g_cam_up: camera.get_up(),
+            g_cam_right: camera.get_right(),
+            g_cam_forward: camera.get_forward(),
+            g_eye: camera.get_position(),
             g_focal_length: 1.67f32,
             g_z_near: 0.0f32,
             g_z_far: 15.0f32,
@@ -76,9 +189,14 @@ fn main() {
 
         target.finish().unwrap();
 
+        let now = Instant::now();
+        let dt = (now.duration_since(then).subsec_micros() as f32) / 1000000.0;
+        then = now;
+
         let mut should_return = false;
         events_loop.poll_events(|e| match e {
             Event::WindowEvent { event, .. } => {
+                camera.update(dt, &event);
                 match event {
                     WindowEvent::CloseRequested => should_return = true,
                     WindowEvent::KeyboardInput { input, .. } => {
